a977b526630e2d7abe0933552c014d44d6e3dbf0
kleodrama <kleodrama@gmail.com>
2025-06-23 10:27:39 +0300

    TEST pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,25 +26,25 @@ def login_screen():
 #         st.session_state.logged_in = False
 #         st.rerun()
 
-login_page = st.Page(login, title="Log in", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
-
-dashboard = st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True)
-page_01 = st.Page("pages/page_01.py", title="page 1", icon=":material/bug_report:")
-page_02 = st.Page("pages/page_02.py", title="page 2", icon=":material/notification_important:")
-
-search = st.Page("pages/search.py", title="Search", icon=":material/search:")
-history = st.Page("pages/history.py", title="History", icon=":material/history:")
-
-if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "Account": [logout_page],
-            "Pages": [dashboard, page_01, page_02],
-            "Tools": [search, history],
-        }
-    )
-else:
-    pg = st.navigation([login_page])
-
-pg.run()
+# login_page = st.Page(login, title="Log in", icon=":material/login:")
+# logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+#
+# dashboard = st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True)
+# page_01 = st.Page("pages/page_01.py", title="page 1", icon=":material/bug_report:")
+# page_02 = st.Page("pages/page_02.py", title="page 2", icon=":material/notification_important:")
+#
+# search = st.Page("pages/search.py", title="Search", icon=":material/search:")
+# history = st.Page("pages/history.py", title="History", icon=":material/history:")
+#
+# if st.session_state.logged_in:
+#     pg = st.navigation(
+#         {
+#             "Account": [logout_page],
+#             "Pages": [dashboard, page_01, page_02],
+#             "Tools": [search, history],
+#         }
+#     )
+# else:
+#     pg = st.navigation([login_page])
+#
+# pg.run()
