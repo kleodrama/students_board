759f220f9ae064b85cf1aad2152baa0e36521fcb
kleodrama <kleodrama@gmail.com>
2025-06-23 10:25:42 +0300

    TEST pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,27 @@ import streamlit as st
 
 st.write("Καλώς ήρθες ...")
 
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Google", on_click=st.login)
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
 
-def login():
-    if st.button("Log in"):
-        st.login(provider="google")
-        st.session_state.logged_in = True
-        st.rerun()
 
-def logout():
-    if st.button("Log out"):
-        st.session_state.logged_in = False
-        st.rerun()
+
+# if "logged_in" not in st.session_state:
+#     st.session_state.logged_in = False
+#
+# def login():
+#     if st.button("Log in"):
+#         st.login()
+#         st.session_state.logged_in = True
+#         st.rerun()
+#
+# def logout():
+#     if st.button("Log out"):
+#         st.session_state.logged_in = False
+#         st.rerun()
 
 login_page = st.Page(login, title="Log in", icon=":material/login:")
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
