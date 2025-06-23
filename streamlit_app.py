78c947f71089d88590ca15971d88a0a3d691d3af
kleodrama <kleodrama@gmail.com>
2025-06-23 10:09:12 +0300

    TEST pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ if "logged_in" not in st.session_state:
 
 def login():
     if st.button("Log in"):
+        st.login(provider="google")
         st.session_state.logged_in = True
         st.rerun()
 
