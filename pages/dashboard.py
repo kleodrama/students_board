f9421980f062d8cc86d14714dfb5a658c8d0f282
kleo <kleodrama@gmail.com>
2025-07-06 17:55:04 +0300

    commit from Pycharm

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,5 +1,41 @@
 
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
 
-st.write("This is Dashboard Page")
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
+for row in df.itertuples():
+    with st.expander(f"({row.season}): {row.name}", expanded=False, icon=None, width="stretch"):
+        st.write(f"({row.season}): {row.name}")
+        st.write(f"id ---> {row.school_id}")
+
+
+with st.expander("Σχολείο 1", expanded=False, icon=None, width="stretch"):
+    "Εδώ θα εμφανίζονται τα τμήματα του σχολείου 1"
+    st.button("α1")
+    st.button("Γ1")
+    st.button("β4")
+    with st.popover("Προσθήκη τμήματος"):
+        st.markdown("Στοιχεία τμήματος 👋")
+        st.text_input("Όνομα τμήματος (π.χ. A1)")
+        st.selectbox("Τάξη",
+                       ("α γυμνασίου", "β γυμνασίου", "γ γυμνασίου",
+                        "Α λυκείου", "Β λυκείου", "Γ λυκείου",
+                        "Α ΕΠΑΛ", "Β ΕΠΑΛ", "Γ ΕΠΑΛ",),)
+        st.button("Προσθήκη")
+
+with st.expander("Σχολείο 2", expanded=False, icon=None, width="stretch"):
+    "Εδώ θα εμφανίζονται τα τμήματα του σχολείου 2"
+    st.button("γ1")
+    # st.button("Γ1")
+    st.button("γ4")
+    with st.popover("Προσθήκη τμήματος"):
+        st.markdown("Στοιχεία τμήματος 👋")
+        st.text_input("Όνομα τμήματος (π.χ. A2)")
+        st.selectbox("Τάξη2",
+                       ("α γυμνασίου", "β γυμνασίου", "γ γυμνασίου",
+                        "Α λυκείου", "Β λυκείου", "Γ λυκείου",
+                        "Α ΕΠΑΛ", "Β ΕΠΑΛ", "Γ ΕΠΑΛ",),)
+        st.button("Προσθήκη2")
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,53 +2,47 @@
 import streamlit as st
 
 
-st.write("Καλώς ήρθες ...")
+# st.write("Καλώς ήρθες ...")
+st.set_page_config(layout="wide")
 
 def login_screen():
     st.header("This app is private.")
     st.subheader("Please log in.")
     st.button("Log in with Google", on_click=st.login)
 
+def logout_screen():
+    st.button("Log out", on_click=st.logout)
 
-if not st.user.is_logged_in:
-    login_screen()
+
+if (not st.user) or (not st.user.is_logged_in):
+    st.session_state.logged_in = False
+    # login_screen()
 else:
-    st.user
+    st.badge(st.user.given_name, icon=":material/check:", color="green")
+    st.session_state.logged_in = True
+
+# st.write(st.session_state)
 
 
-# if "logged_in" not in st.session_state:
-#     st.session_state.logged_in = False
-#
-# def login():
-#     if st.button("Log in"):
-#         st.login()
-#         st.session_state.logged_in = True
-#         st.rerun()
-#
-# def logout():
-#     if st.button("Log out"):
-#         st.session_state.logged_in = False
-#         st.rerun()
+login_page = st.Page(login_screen, title="Log in", icon=":material/login:")
+logout_page = st.Page(logout_screen, title="Log out", icon=":material/logout:")
 
-# login_page = st.Page(login, title="Log in", icon=":material/login:")
-# logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
-#
-# dashboard = st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True)
-# page_01 = st.Page("pages/page_01.py", title="page 1", icon=":material/bug_report:")
-# page_02 = st.Page("pages/page_02.py", title="page 2", icon=":material/notification_important:")
-#
-# search = st.Page("pages/search.py", title="Search", icon=":material/search:")
-# history = st.Page("pages/history.py", title="History", icon=":material/history:")
-#
-# if st.session_state.logged_in:
-#     pg = st.navigation(
-#         {
-#             "Account": [logout_page],
-#             "Pages": [dashboard, page_01, page_02],
-#             "Tools": [search, history],
-#         }
-#     )
-# else:
-#     pg = st.navigation([login_page])
-#
-# pg.run()
+dashboard = st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True)
+page_01 = st.Page("pages/page_01.py", title="page 1", icon=":material/bug_report:")
+page_02 = st.Page("pages/page_02.py", title="page 2", icon=":material/notification_important:")
+
+search = st.Page("pages/search.py", title="Search", icon=":material/search:")
+history = st.Page("pages/history.py", title="History", icon=":material/history:")
+
+if st.session_state.logged_in:
+    pg = st.navigation(
+        {
+            "Account": [logout_page],
+            "Pages": [dashboard, page_01, page_02],
+            "Tools": [search, history],
+        }
+    )
+else:
+    pg = st.navigation([login_page])
+
+pg.run()
